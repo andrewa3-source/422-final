b2ac4735ceec21b9eeb25cf30b65fd0d62c44f8f
Andrew Ahrenkiel <andrewa3@iastate.edu>
2025-05-12 19:27:28 -0500

    app update

--- project4/main.py
+++ project4/main.py
@@ -1,29 +1,107 @@
-import datetime
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import (
+    LoginManager,
+    login_user,
+    login_required,
+    logout_user,
+    current_user,
+)
+from config import Config
+import db
 
 app = Flask(__name__)
+app.config.from_object(Config)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_user_by_id(user_id)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("gallery"))
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user: db.User = db.get_user_by_username_and_password(username, password)
+
+        if user:
+            login_user(user)
+            return redirect(url_for("gallery"))
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("gallery"))
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if db.get_user_by_username(username):
+            return render_template("register.html", error="Username already exists")
+
+        db.create_user(username, password)
+
+        return redirect(url_for("login"))
+
+    return render_template("register.html")
+
+
+@app.route("/upload", methods=["GET", "POST"])
+@login_required
+def upload():
+    if request.method == "POST":
+        file = request.files["file"]
+        description = request.form["description"]
+
+        db.create_photo(file, description, current_user.id)
+        return redirect(url_for("gallery"))
+
+    return render_template("upload.html")
 
 
 @app.route("/")
-def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [
-        datetime.datetime(2018, 1, 1, 10, 0, 0),
-        datetime.datetime(2018, 1, 2, 10, 30, 0),
-        datetime.datetime(2018, 1, 3, 11, 0, 0),
-    ]
+@login_required
+def gallery():
+    photos_list = db.get_photos(request.args.get("search", ""))
 
-    return render_template("index.html", times=dummy_times)
+    return render_template(
+        "gallery.html",
+        photos=photos_list,
+        gcs_bucket_name=app.config["GCS_BUCKET_NAME"],
+    )
+
+
+@app.route("/download/<filename>")
+@login_required
+def download(filename):
+    return redirect(db.get_photo_url(filename))
+
+
+@app.route("/delete/<photo_id>", methods=["POST"])
+@login_required
+def delete(photo_id):
+    db.delete_photo(photo_id, current_user.id)
+
+    return redirect(url_for("gallery"))
 
 
 if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=8080)
